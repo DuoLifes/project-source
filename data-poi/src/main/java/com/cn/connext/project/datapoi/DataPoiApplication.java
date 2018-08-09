c6c305373e1435aa89adbfc77904a623251da7e7
DUO <1564900966@qq.com>
2018-08-09 09:46:56 +0800

    data-poi

--- data-poi/src/main/java/com/cn/connext/project/datapoi/DataPoiApplication.java
+++ data-poi/src/main/java/com/cn/connext/project/datapoi/DataPoiApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@ConnextMicroServiceApplication(scanBasePackages = {"com.cn.connext.project.startelasticsearch"})
+@ConnextMicroServiceApplication
+//(scanBasePackages = {"com.cn.connext.project.startelasticsearch"})
 public class DataPoiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DataPoiApplication.class, args);
