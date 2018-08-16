30a2f76c5084133d0579a4696fa358314b4a35c4
Duo <1564900966@qq.com>
2018-08-17 00:51:57 +0800

    multi-mongoDB-增删改查全套方法

--- multi-datasource/src/main/java/com/cn/connext/project/multidatasource/service/mongo/MediaInfoSourceService.java
+++ multi-datasource/src/main/java/com/cn/connext/project/multidatasource/service/mongo/MediaInfoSourceService.java
@@ -2,14 +2,20 @@ package com.cn.connext.project.multidatasource.service.mongo;
 
 import com.cn.connext.project.multidatasource.entity.mongo.MediaInfoSource;
 import com.cn.connext.project.multidatasource.repository.mongo.MediaInfoSourceRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapreduce.GroupBy;
+import org.springframework.data.mongodb.core.mapreduce.GroupByResults;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -32,7 +38,7 @@ public class MediaInfoSourceService {
     //创建文档集合
     public void createList(){
         List<MediaInfoSource> list=new ArrayList<>();
-        for(int i=0;i<10;i++){
+        for(int i=1;i<=10;i++){
             MediaInfoSource mediaInfoSource=new MediaInfoSource();
             mediaInfoSource.toString(mediaInfoSource,i);
             list.add(mediaInfoSource);
@@ -40,9 +46,24 @@ public class MediaInfoSourceService {
         mediaInfoSourceRepository.batchCreate(list);
     }
 
-    //删除文档
+    //根据id删除文档
     public void delete(String id){
         mediaInfoSourceRepository.delete(id);
+    }
+
+    //修改文档
+    public void update(Object name,Object remark){
+        Criteria orCriteria=new Criteria().orOperator(Criteria.where("name").is(name),Criteria.where("remark").is(remark));
+        Query query=new Query(orCriteria);
+        Update update=new Update();
+        Object upName="陈奕迅";
+        Object upRemark="歌神";
+        update.rename("type","社会王");//用来指定一个字段的新名字
+        update.set("name",upName);
+        update.set("remark",upRemark);
+        mediaInfoSourceRepository.update(query,update);
+        //updateFirst修改符合条件的第一条文档
+        //mongoTemplate.findAndModify()
     }
 
     //根据id查询文档
@@ -50,21 +71,67 @@ public class MediaInfoSourceService {
       return  mediaInfoSourceRepository.findOne(id);
     }
 
-    //条件查询文档  Criteria构造查询条件
+    //单条件查询文档  Criteria构造查询条件
     public List<MediaInfoSource> findDocumentByName(Object name){
         return  mongoTemplate.find(new Query(Criteria.where("name").is(name)),MediaInfoSource.class);
     }
 
     //多条件查询 Criteria构造查询条件
     public List<MediaInfoSource> findOneByNameAndRemark(Object name,Object remark){
-        Object a=null;
         //And条件查询语句构造
         //Criteria andCriteria=Criteria.where("name").is(name).and("remark").is(remark);
         //范围查询语句构造
-        Criteria rangeCriteria=Criteria.where("createTime").lt(a);
+        //Criteria rangeCriteria=Criteria.where("createTime").gt(begin).lt(end);
         //Or条件查询语句构造
         Criteria orCriteria=new Criteria().orOperator(Criteria.where("name").is(name),Criteria.where("remark").is(remark));
-        return mongoTemplate.find(new Query(rangeCriteria),MediaInfoSource.class);
+        return mongoTemplate.find(new Query(orCriteria),MediaInfoSource.class);
     }
 
+    //案例查询（Example）
+    public Page<MediaInfoSource> findByExample(MediaInfoSource mediaInfoSource){
+        mediaInfoSource.setId(null);//无关查询条件置空
+        mediaInfoSource.setCreateTime(null);//初始化实体默认值清空
+        Example <MediaInfoSource> example=Example.of(mediaInfoSource);
+        return mediaInfoSourceRepository.findAll(example,new PageRequest(0,5));
+    }
+
+    //自定义案例查询
+    public Page<MediaInfoSource> findByCustomExample(MediaInfoSource mediaInfoSource){
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withMatcher("name", ExampleMatcher.GenericPropertyMatchers.startsWith())//模糊查询匹配开头，即{name}%
+                .withMatcher("type" ,ExampleMatcher.GenericPropertyMatchers.contains())//全部模糊查询，即%{type}%
+                .withMatcher("remark" ,ExampleMatcher.GenericPropertyMatchers.endsWith())//模糊查询匹配结尾，即%{remark}
+                .withIgnorePaths("id")//忽略字段，即不管id是什么值都不加入查询条件
+                .withIgnorePaths("createTime");//忽略字段，即不管updateTime是什么值都不加入查询条件
+        Example<MediaInfoSource> example = Example.of(mediaInfoSource ,matcher);
+        return  mediaInfoSourceRepository.findAll(example,new PageRequest(0,5));
+    }
+
+    //查询数量
+    public Long count(){
+        return mediaInfoSourceRepository.count();
+    }
+
+    //按条件查询数量
+    public Long countByExample(MediaInfoSource mediaInfoSource){
+        mediaInfoSource.setId(null);//无关查询条件置空
+        mediaInfoSource.setCreateTime(null);//初始化实体默认值清空
+        Example <MediaInfoSource> example=Example.of(mediaInfoSource);
+        return mediaInfoSourceRepository.count(example);
+    }
+
+    //分组聚合（可以显示其他字段）
+    public GroupByResults<MediaInfoSource> group(){
+        GroupBy groupBy = GroupBy.key("name","remark").initialDocument("{createTime:[],count:0}")
+                .reduceFunction("function(doc, prev){" + "prev.createTime.push(doc.createTime);" + "prev.count+=1}");
+        return mongoTemplate.group("mediaInfoSource", groupBy,MediaInfoSource.class);
+    }
+
+    //条件查询再分组聚合
+    public GroupByResults<MediaInfoSource> findByGroup(Object name, Object remark){
+        GroupBy groupBy = GroupBy.key("name","remark").initialDocument("{createTime:[],count:0}")
+                .reduceFunction("function(doc, prev){" + "prev.createTime.push(doc.createTime);" + "prev.count+=1}");
+        Criteria orCriteria=new Criteria().orOperator(Criteria.where("name").is(name),Criteria.where("remark").is(remark));
+        return mongoTemplate.group(orCriteria,"mediaInfoSource",groupBy,MediaInfoSource.class);
+    }
 }
--- multi-datasource/src/main/java/com/cn/connext/project/multidatasource/webapi/mongo/MediaInfoSourceAPI.java
+++ multi-datasource/src/main/java/com/cn/connext/project/multidatasource/webapi/mongo/MediaInfoSourceAPI.java
@@ -3,6 +3,10 @@ package com.cn.connext.project.multidatasource.webapi.mongo;
 import com.cn.connext.project.framework.annotation.WebAPI;
 import com.cn.connext.project.multidatasource.entity.mongo.MediaInfoSource;
 import com.cn.connext.project.multidatasource.service.mongo.MediaInfoSourceService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.mongodb.core.mapreduce.GroupBy;
+import org.springframework.data.mongodb.core.mapreduce.GroupByResults;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,6 +45,11 @@ public class MediaInfoSourceAPI {
         return mediaInfoSourceService.findDocumentById(id);
     }
 
+    @GetMapping("/update")
+    public void update(@RequestParam String name,@RequestParam String remark){
+        mediaInfoSourceService.update(name,remark);
+    }
+
     //条件查询文档 Criteria构造查询条件
     @GetMapping("/findDocumentByName")
     public List<MediaInfoSource> findOne(@RequestParam String name){
@@ -51,5 +60,41 @@ public class MediaInfoSourceAPI {
     @GetMapping("/findOneByNameAndRemark")
     public List<MediaInfoSource> findOneByNameAndRemark(@RequestParam String name,@RequestParam String remark){
         return mediaInfoSourceService.findOneByNameAndRemark(name,remark);
+    }
+
+    //案例查询（Example）
+    @PostMapping("/findByExample")
+    public Page<MediaInfoSource> findByExample(@RequestBody MediaInfoSource mediaInfoSource){
+        return mediaInfoSourceService.findByExample(mediaInfoSource);
+    }
+
+    //自定义案例查询
+    @PostMapping("/findByCustomExample")
+    public Page<MediaInfoSource> findByCustomExample(@RequestBody MediaInfoSource mediaInfoSource){
+        return mediaInfoSourceService.findByCustomExample(mediaInfoSource);
+    }
+
+    //查询数量
+    @GetMapping("/count")
+    public Long count(){
+        return mediaInfoSourceService.count();
+    }
+
+   //按条件查询数量
+   @PostMapping("/countByExample")
+    public Long countByExample(@RequestBody MediaInfoSource mediaInfoSource){
+        return mediaInfoSourceService.countByExample(mediaInfoSource);
+    }
+
+    //分组聚合（可以显示其他字段）
+    @GetMapping("/group")
+    public GroupByResults<MediaInfoSource> group(){
+        return mediaInfoSourceService.group();
+    }
+
+    //条件查询再分组聚合
+    @GetMapping("/findByGroup")
+    public GroupByResults<MediaInfoSource> findByGroup(@RequestParam String name, @RequestParam String remark){
+        return mediaInfoSourceService.findByGroup(name,remark);
     }
 }
