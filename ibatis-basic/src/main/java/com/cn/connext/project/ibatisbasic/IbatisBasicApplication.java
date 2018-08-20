37e8403f68b342ca3f6583c632fed847237f7a50
Duo <1564900966@qq.com>
2018-08-20 23:46:09 +0800

    ibatis-basic-mybatis-关联查询

---
+++ ibatis-basic/src/main/java/com/cn/connext/project/ibatisbasic/IbatisBasicApplication.java
@@ -0,0 +1,11 @@
+package com.cn.connext.project.ibatisbasic;
+
+import com.cn.connext.project.framework.annotation.ConnextMicroServiceApplication;
+import org.springframework.boot.SpringApplication;
+
+@ConnextMicroServiceApplication
+public class IbatisBasicApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IbatisBasicApplication.class, args);
+	}
+}
