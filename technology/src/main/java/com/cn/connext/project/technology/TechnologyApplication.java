15c472e65468f9ea8d8ff55e1a6aa8a5777f8bf6
DUO <1564900966@qq.com>
2018-08-28 09:31:36 +0800

    technology

---
+++ technology/src/main/java/com/cn/connext/project/technology/TechnologyApplication.java
@@ -0,0 +1,15 @@
+package com.cn.connext.project.technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+
+@SpringBootApplication
+@EnableRetry
+public class TechnologyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TechnologyApplication.class, args);
+	}
+}
---
+++ technology/src/test/java/com/cn/connext/project/technology/TechnologyApplicationTests.java
@@ -0,0 +1,23 @@
+//package com.cn.connext.project.technology;
+//
+//import com.cn.connext.project.technology.repository.TestRepository;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import javax.annotation.Resource;
+//import java.util.List;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class TechnologyApplicationTests {
+//
+//	@Resource
+//	private TestRepository testRepository;
+//	@Test
+//	public void contextLoads() {
+//		List<com.cn.connext.project.technology.entity.Test> list=testRepository.findAll();
+//		System.out.println(list.get(0).toString());
+//	}
+//
+//}
