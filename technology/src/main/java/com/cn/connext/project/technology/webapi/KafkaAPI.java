3a2aa939d1304d75f9ce394f35ff3dddb6566488
DUO <1564900966@qq.com>
2018-09-03 09:41:16 +0800

    technology-修改

--- technology/src/main/java/com/cn/connext/project/technology/webapi/KafkaAPI.java
+++ technology/src/main/java/com/cn/connext/project/technology/webapi/KafkaAPI.java
@@ -2,17 +2,19 @@ package com.cn.connext.project.technology.webapi;
 
 import com.cn.connext.project.framework.annotation.WebAPI;
 import com.cn.connext.project.technology.config.KafkaConfig;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.annotation.Resource;
+
 
 @WebAPI("/api/technology/kafka")
 public class KafkaAPI {
 
     @Resource
     private KafkaConfig kafkaConfig;
+    private static final org.slf4j.Logger logger= LoggerFactory.getLogger(KafkaAPI.class);
 
     /**
      * 向kafka发送线索
@@ -22,7 +24,11 @@ public class KafkaAPI {
      */
     @GetMapping("/send")
     public void sendmessage(@RequestParam String topicName, @RequestParam String message) {
+        try {
         KafkaTemplate<String, String> kafkaTemplate = kafkaConfig.kafkaTemplate();
         kafkaTemplate.send(topicName, message);
+    }catch (Exception e){
+            logger.info("消息发送失败:"+e);
+        }
     }
 }
--- technology/src/main/java/com/cn/connext/project/technology/webapi/MediaAPI.java
+++ technology/src/main/java/com/cn/connext/project/technology/webapi/MediaAPI.java
@@ -13,6 +13,16 @@ public class MediaAPI {
     @Resource
     private MediaRepository mediaRepository;
 
+    @GetMapping("/create")
+    public Media create(){
+        Media media=new Media();
+        media.setMediaTypeId("433E44B0-C6B3-4E15-8F1E-C626BA248A8C");
+        media.setCode("SA09776");
+        media.setName("测试媒体");
+        media.setRemark("测试媒体备注");
+        return mediaRepository.save(media);
+    }
+
     @GetMapping("/findAll")
     public List<Media> findAll() {
         return mediaRepository.findAll();
