cab5f7747b6d42827b21df6288dc7842ddd6de9c
Duo <1564900966@qq.com>
2018-09-27 21:14:46 +0800

    demo

--- demo/src/test/java/com/cn/connext/project/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/cn/connext/project/demo/DemoApplicationTests.java
@@ -2,11 +2,6 @@ package com.cn.connext.project.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-<<<<<<< HEAD
-=======
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
->>>>>>> origin/master
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,15 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
-<<<<<<< HEAD
 	@Test
 	public void contextLoads() {
-=======
-	private static final Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
-
-	@Test
-	public void contextLoads(){
->>>>>>> origin/master
 	}
 
 }
