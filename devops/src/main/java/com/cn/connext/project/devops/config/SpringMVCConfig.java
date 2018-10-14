f61c77b8589693d706e43e246c680b52069e1aad
Duo <1564900966@qq.com>
2018-10-14 21:22:35 +0800

    devops

--- devops/src/main/java/com/cn/connext/project/devops/config/SpringMVCConfig.java
+++ devops/src/main/java/com/cn/connext/project/devops/config/SpringMVCConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
--- devops/src/main/java/com/cn/connext/project/devops/webapi/LoginAPI.java
+++ devops/src/main/java/com/cn/connext/project/devops/webapi/LoginAPI.java
@@ -1,17 +1,14 @@
 package com.cn.connext.project.devops.webapi;
 
-import com.cn.connext.project.framework.annotation.WebAPI;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/start")
+@RequestMapping("/test")
 public class LoginAPI {
 
-    @RequestMapping("/aaa")
-    public String bbb(){
+    @RequestMapping("/login")
+    public String login(){
         return "login";
     }
 }
